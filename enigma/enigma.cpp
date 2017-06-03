0f1cd6f3c7bd9f05e7e3b8c673cc702782fb5ff7
c3ll256 <candycell256@gmail.com>
2017-06-03 14:04:06 +0800

    Revert "模型版本"

--- enigma/enigma.cpp
+++ enigma/enigma.cpp
@@ -7,8 +7,23 @@ enigma::enigma(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(ClickButton1()));
+	connect(ui.pushButton_2, SIGNAL(clicked()), this, SLOT(ClickButton2()));
 }
 
 enigma::~enigma()
 {
+}
+
+void enigma::ClickButton1()
+{
+	img.load("sample.png");
+	deco = dcoder.decodeImage(img);
+	QMessageBox inmessage;
+	inmessage.about(NULL, "decode", deco);
+}
+
+void enigma::ClickButton2()
+{
+	ui.widget->generateString("yooooooo");
 }
--- enigma/main.cpp
+++ enigma/main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-	//Ö÷´°Ìå¼ÓÔØ
 	QApplication a(argc, argv);
 	enigma w;
 	w.show();
